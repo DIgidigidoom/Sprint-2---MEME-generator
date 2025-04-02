abd1b558011d23d0b2babfab9c76450fdafe980e
Digidigidoom <tomshahar91@gmail.com>
2025-04-02 11:28:15 +0300

    Add Gallery

--- js/gallery.controler.js
+++ js/gallery.controler.js
@@ -1,1 +1,15 @@
 'use strict'
+
+function onInitGallery() {
+    renderImgs()
+}
+
+function renderImgs() {
+    const imgs = getImgs()
+    let strHTML = imgs.map(img =>
+        `<img src="${img.url}" id="${img.id}" onclick="onSelectPic(${img.id})">`
+    )
+    debugger
+    document.querySelector('.gallery-container').innerHTML = strHTML.join('')
+
+}
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,8 +1,27 @@
 'use strict'
 
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gImgs = [
+    { id: 1, url: 'Images/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'Images/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: 'Images/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: 'Images/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: 'Images/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: 'Images/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: 'Images/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: 'Images/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: 'Images/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: 'Images/10.jpg', keywords: ['funny', 'cat'] },
+    { id: 11, url: 'Images/11.jpg', keywords: ['funny', 'cat'] },
+    { id: 12, url: 'Images/12.jpg', keywords: ['funny', 'cat'] },
+    { id: 13, url: 'Images/13.jpg', keywords: ['funny', 'cat'] },
+    { id: 14, url: 'Images/14.jpg', keywords: ['funny', 'cat'] },
+    { id: 15, url: 'Images/15.jpg', keywords: ['funny', 'cat'] },
+    { id: 16, url: 'Images/16.jpg', keywords: ['funny', 'cat'] },
+    { id: 17, url: 'Images/17.jpg', keywords: ['funny', 'cat'] },
+    { id: 18, url: 'Images/18.jpg', keywords: ['funny', 'cat'] },
+]
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
         {
@@ -13,3 +32,16 @@ var gMeme = {
     ]
 }
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getImgs() {
+    return gImgs
+}
+
+function createImg() {
+    return {
+        id: 1,
+        url:
+            'img/1.jpg',
+        keywords: ['funny', 'cat']
+    }
+}
